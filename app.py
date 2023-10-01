431037c6e8b4beeddaf061757a71bb650a80d89c
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-10-01 22:05:20 +0530

    Add few parameters

--- app.py
+++ app.py
@@ -3,6 +3,19 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
+# User's choice
+LANGUAGE = None
+OPTIONS_LANG = ["English",
+                "Hindi",
+                "Marathi",
+                "Gujarati",
+                "Kannada",
+                "Tamil",
+                ]
+
+HELLO = "Hi! Welcome to eVakil, your digital lawyer."
+
+
 @app.route('/')
 def index():
     return render_template("index.html")
