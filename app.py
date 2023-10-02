e64c80f58b0ad8c32692ece81c75c8f4da606c63
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-10-02 17:01:00 +0530

    General purpose make_audio

--- app.py
+++ app.py
@@ -21,33 +21,31 @@ LANG_MAP = {"English": "en",  # Language mapping
             "Telugu": "te",
             "Malayalam": "ml",
             "Bengali": "bn",
-            "Punjabi": "pa",
             "Japanese": "ja",
             }
 
-HELLO = {
-    "English": "Hi! Welcome to \"eVakil\", your digital lawyer and assistant.",
-}
+HELLO = "Hi! Welcome to \"eVakil\", your digital lawyer and assistant."
 
 
 def get_hello_message(lang):
 
     translator = Translator()
     tranlation = translator.translate(
-        HELLO['English'], src="en", dest=LANG_MAP[lang])
+        HELLO, src="en", dest=LANG_MAP[lang])
 
     return tranlation.text
 
 
-def make_audio(text, lang):
+def make_audio(text, lang, audio_path=None):
 
-    audio_path = f"./static/audio/hello_{lang}.mp3"
+    if audio_path is None:
+        raise ValueError("Audio path not provided.")
 
     # Audio file not present or REGEN_AUDIO flag is True
     if not os.path.exists(audio_path) or REGEN_AUDIO:
+
         tts = gTTS(text=text, lang=LANG_MAP[lang], slow=False)
         tts.save(audio_path)
-        # print(f"File saved successfully in {lang} language.")
 
     return audio_path
 
@@ -63,16 +61,16 @@ def index():
         # Get user's choice
         global user_preference
         user_preference["language"] = request.form.get('language')
-        # print(f"Language selected: {LANGUAGE}")
 
         # Get hello message in selected language
         result["hello_message"] = get_hello_message(
             user_preference["language"])
-        # print(f"Hello message: {result['hello_message']}")
 
         # Make audio file
         result["hello_audio"] = make_audio(
-            result["hello_message"], user_preference["language"])
+            result["hello_message"],
+            user_preference["language"],
+            audio_path=f"./static/audio/hello_{user_preference['language']}.mp3")
 
     return render_template("index.html",
                            options=LANG_MAP.keys(),
